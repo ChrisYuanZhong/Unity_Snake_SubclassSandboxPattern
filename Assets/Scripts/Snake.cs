bdbc4baf74ff3d067e1ad82cc2024c9dfbebf9b9
Chris Yuan Zhong <chrisyuanzhong@outlook.com>
2024-01-16 18:06:50 -0700

    Let UI scale with screen size. And 'ESC' to quit

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -54,6 +54,11 @@ public class Snake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit(0);
+        }
+
         if (Input.GetKeyDown(KeyCode.R) && !_isAlive)
         {
             Restart();
